aebd10c3d3596b5bc1bd2fba86e8696192f683c1
Defl1ks <44494108+yarik1367@users.noreply.github.com>
2023-11-24 08:16:03 +0300

    MethodGets

--- index.rb
+++ index.rb
@@ -1,3 +1,11 @@
 Name = "Yarik"
 age = 16
-print ("Меня зовут " + Name)
+print ("Меня зовут " + Name + "\n")
+# puts("Enter your pass: ");
+# pass = gets
+# print("is it ypur pass? - " + pass)
+print("enter first num ")
+x = gets
+print("enter first second ")
+y = gets
+puts(x.to_i + y.to_i)
